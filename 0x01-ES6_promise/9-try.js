63d06636396069dbcec2f18249f3a9781eee1ce6
HaxorLeoul <workinehw94@gmail.com>
2022-12-20 11:54:25 -0800

    Commit

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,15 @@
+export default function guardrail(mathFunction) {
+  const queue = [];
+
+  try {
+    queue.push(mathFunction());
+    queue.push('Guardrail was processed');
+  } catch (err) {
+    queue.push(err.toString());
+    queue.push('Guardrail was processed');
+  }
+
+  return queue;
+}
+Footer
+© 2022 GitHub
